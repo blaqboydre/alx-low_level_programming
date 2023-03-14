860076d147182f7cc98f60105539d79254efe861
Andrews Odame <odameandy8989@gmail.com>
2023-03-13 19:13:24 -0700

    Static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
-void _puts(char *str)
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
 
+/**
+ * _puts - print a string to stdout
+ * @str: char array string type
+ * Description: Can only use _putchar
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
